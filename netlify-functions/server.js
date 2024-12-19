dead50048e83860c9c6c40d2ff6e33e6bf65e1f6
rudalkunwar <ruzalkunwar@gmail.com>
2024-12-19 11:31:38 +0545

    fixes

--- netlify-functions/server.js
+++ netlify-functions/server.js
@@ -1,11 +1,16 @@
 // netlify-functions/server.js
-const { handler } = require('@netlify/functions');
-const express = require('express');
+// const { handler } = require('@netlify/functions');
+const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
+const router = express.Router();
 
-app.get("/", (req, res) => {
-  res.send('Hello from Netlify Server!');
+router.get("/", (req, res) => {
+  res.send("Hello from Netlify Server!");
 });
 
-// Wrap the Express app into the Netlify handler
-exports.handler = handler(app);
+// // Wrap the Express app into the Netlify handler
+// exports.handler = handler(app);
+
+app.use("/.netlify/functions/api", router);
+module.exports.handler = serverless(app);
